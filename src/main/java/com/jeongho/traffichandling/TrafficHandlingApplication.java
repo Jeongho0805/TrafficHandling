f266181b12f012378461f889309e232ce973dda8
JeonghoPark <pjhpjh0805@naver.com>
2023-08-24 18:55:18 +0900

    first commit

---
+++ src/main/java/com/jeongho/traffichandling/TrafficHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.jeongho.traffichandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficHandlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeongho/traffichandling/TrafficHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeongho.traffichandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
